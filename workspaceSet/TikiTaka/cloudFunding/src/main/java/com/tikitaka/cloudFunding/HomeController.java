c4b0c37f324a6d480f81dd47622f86ea4426265a
YSJ <ysjhmtb@gmail.com>
2018-08-05 13:15:24 +0900

    ongoing tikitaka join

---
+++ workspaceSet/TikiTaka/cloudFunding/src/main/java/com/tikitaka/cloudFunding/HomeController.java
@@ -0,0 +1,45 @@
+package com.tikitaka.cloudFunding;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "index.do", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+
+
+		System.out.println("sd");
+		System.out.println("123");
+		System.out.println("333");
+		System.out.println("444");
+		System.out.println("정인 수정2");
+		System.out.println("지상");
+		System.out.println("가용");
+
+		return "home";
+	}
+	
+}
---
+++ workspaceSet/TikiTaka/cloudFunding/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<c:import url="common/header.jsp"></c:import>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Hello world!  
+</h1>
+
+	<a href="loginPage.do">login</a>
+	<a href="joinPage.do">join</a>
+	<a href="projectForm.do">projectForm</a>
+</body>
+</html>
